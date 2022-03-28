74a0f3210f1200f03f1176763c0bde226a3149cd
dukker <fred.dukker@kpnmail.nl>
2022-03-28 08:15:17 +0200

    fix: fix the formatting

--- release.config.js
+++ release.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/dukker/react-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-  ],
+  branches: 'main',
+  repositoryUrl: 'https://github.com/dukker/react-app',
+  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github'],
 };
