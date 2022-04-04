6678b64bbd2f36b20ebb2194e0b8b7b3c3ff410f
dukker <fred.dukker@kpnmail.nl>
2022-04-04 19:15:16 +0200

    feat: added action create issue

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -9,7 +9,7 @@ async function run() {
     const assignees = core.getInput('assignees');
     const octokit = new github.getOctokit(token);
 
-    const response = await octokit.issues.create({
+    const response = await octokit.rest.issues.create({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       title,
