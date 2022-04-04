9c1f7eea262569a22f3fb9a5e205178f813a3daf
dukker <fred.dukker@kpnmail.nl>
2022-04-04 18:19:29 +0200

    feat: added functions of core

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -882,13 +882,21 @@ const github = __webpack_require__(469);
 
 try {
   // throw(new Error('some error message'));
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+  core.setSecret('This text will be masked in the log with ***');
   const name = core.getInput('who-to-great');
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput('time', time.toTimeString());
 
-  console.log(JSON.stringify(github, null, '\t'));
+  core.startGroup('Logging github object');
+  // console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable('HELLO_VAR', 'hello my name is');
 } catch (error) {
   core.setFailed('This is the message why it fail. Is needed to let github know that it failed; ' + error.message);
 }
